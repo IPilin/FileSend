cd1f8e1fa2e8057bdf145d2840845b855d3a8e08
botnavas <maksymmelkov@yandex.ru>
2021-11-22 21:48:52 +0300

    added CryptoClass

---
+++ FileSend/Classes/CryptoClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+namespace FileSend.Classes
+{
+    class CryptoClass
+    {
+        public byte[] EncryptedMessage { get; private set; }
+        public byte[] PureMessage { get; private set; }
+
+        private RSACryptoServiceProvider Key;
+
+        CryptoClass(byte[] message, RSACryptoServiceProvider key)
+        { 
+        }
+    }
+}
