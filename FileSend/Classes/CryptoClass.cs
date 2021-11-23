41ce8080acfed467bcc2c0f5584073b70a31f1a8
Def0lt <lipilinalexey@gmail.com>
2021-11-23 22:45:34 +0300

    crypto

--- FileSend/Classes/CryptoClass.cs
+++ FileSend/Classes/CryptoClass.cs
@@ -4,32 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
-
+using System.IO;
 
 namespace FileSend.Classes
 {
     class CryptoClass
     {
-        public byte[] Key { get; set; }
+        public byte[] AesKey { get; set; }
+
 
         CryptoClass()
         {
-            Key = GenerateKey();
+            AesKey = GenerateKey();
         }
 
         public byte[] Encrypt(byte[] data)
         {
-            return null;
+            if (data == null)
+                return null;
+
+            if (AesKey == null)
+            {
+
+            }
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.GenerateIV();
+
+                ICryptoTransform cryptoTransform = aes.CreateEncryptor(AesKey, aes.IV);
+
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
+                    {
+                        ms.Write(data, 0, data.Length);
+
+                        return ms.ToArray();
+                    }
+                }
+            }
         }
 
         public byte[] Decrypt(byte[] data)
         {
-            return null;
+            if (data == null)
+                return null;
+
+            using (Aes aes = Aes.Create())
+            {
+                ICryptoTransform cryptoTransform = aes.CreateDecryptor(AesKey, null);
+
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Read))
+                    {
+                        cs.Read(data, 0, data.Length);
+
+                        return ms.ToArray();
+                    }
+                }
+            }
         }
 
         public byte[] GenerateKey()
         {
-            return null;
+            using (Aes aes = Aes.Create())
+            {
+                aes.GenerateKey();
+
+                return aes.Key;
+            }
         }
     }
 }
