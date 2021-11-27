f7637ce4f9648c72615763333001bc25bae3ffb0
botnavas <maksymmelkov@yandex.ru>
2021-11-27 23:25:42 +0300

    Reworked CryptoClass

--- FileSend/Classes/CryptoClass.cs
+++ FileSend/Classes/CryptoClass.cs
@@ -21,51 +21,20 @@ namespace FileSend.Classes
         public byte[] Encrypt(byte[] data)
         {
             if (data == null)
-                return null;
+                return new byte[0];
 
-            if (AesKey == null)
+            using (var aes = Aes.Create())
             {
-
-            }
-
-            byte[] toEncrypt;
-
-            using (var ms = new MemoryStream())
-            {
-                using (var bw = new BinaryWriter(ms))
-                {
-                    bw.Write(data.Length);
-                    bw.Write(data);
-                }
-
-                toEncrypt = ms.ToArray();
-            }
-
-            using (Aes aes = Aes.Create())
-            {
-                byte[] encryptedData;
-                aes.GenerateIV();
-
-                ICryptoTransform encryptor = aes.CreateEncryptor(AesKey, aes.IV);
-
                 using (var ms = new MemoryStream())
-                {
+                {                   
+                    var encryptor = aes.CreateEncryptor(AesKey, aes.IV);
                     using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
                     {
-                        cs.Write(toEncrypt, 0, toEncrypt.Length); 
-                    }
-
-                    encryptedData = ms.ToArray();
-                }
-
-                byte[] result = new byte[encryptedData.Length + aes.IV.Length];
-
-                using (var ms = new MemoryStream(result))
-                {
-                    ms.Seek(0, SeekOrigin.Begin);
-                    ms.Write(encryptedData, 0, encryptedData.Length);
-                    ms.Write(aes.IV, 0, aes.IV.Length);
-
+                        cs.Write(BitConverter.GetBytes(data.Length), 0, 4);
+                        cs.Write(data, 0, data.Length);
+                        cs.FlushFinalBlock();
+                        ms.Write(aes.IV, 0, aes.IV.Length);
+                    }                    
                     return ms.ToArray();
                 }
             }
@@ -74,44 +43,27 @@ namespace FileSend.Classes
         public byte[] Decrypt(byte[] data)
         {
             if (data == null)
-                return null;
+                return new byte[0];
 
-            byte[] IV;
-            byte[] toDecrypt;
-            byte[] decrypted;
+            byte[] IV = new byte[16];
+            for (int i = data.Length - 16; i < data.Length; i++)
+            {
+                IV[i - data.Length + 16] = data[i];
+            }
 
             using (var ms = new MemoryStream(data))
             {
-                using (var br = new BinaryReader(ms))
+                using (Aes aes = Aes.Create())
                 {
-                    ms.Seek(0, SeekOrigin.Begin);
-                    toDecrypt = br.ReadBytes(data.Length - 16);
-                    IV = br.ReadBytes(16);
-                }
-            }
-
-            using (Aes aes = Aes.Create())
-            {
-                ICryptoTransform decryptor = aes.CreateDecryptor(AesKey, IV);
-
-                using (var ms = new MemoryStream(toDecrypt))
-                {
+                    var decryptor = aes.CreateDecryptor(AesKey, IV);
                     using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
                     {
-                        cs.Read(toDecrypt, 0, toDecrypt.Length);
+                        using (var br = new BinaryReader(cs))
+                        {
+                            int count = br.ReadInt32();
+                            return br.ReadBytes(count);
+                        }
                     }
-
-                    decrypted = ms.ToArray();
-                }
-            }
-
-            using (var ms = new MemoryStream(decrypted))
-            {
-                using (var br = new BinaryReader(ms))
-                {
-                    int toRead = br.ReadInt32();
-
-                    return br.ReadBytes(toRead);
                 }
             }
         }
@@ -121,7 +73,6 @@ namespace FileSend.Classes
             using (Aes aes = Aes.Create())
             {
                 aes.GenerateKey();
-
                 return aes.Key;
             }
         }
