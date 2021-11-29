055631383308e0797e1f840180bcd4ca8aac14a7
botnavas <maksymmelkov@yandex.ru>
2021-11-29 21:46:26 +0300

    Added RSA

--- FileSend/Classes/CryptoClass.cs
+++ FileSend/Classes/CryptoClass.cs
@@ -8,14 +8,27 @@ using System.IO;
 
 namespace FileSend.Classes
 {
-    public class CryptoClass
+    public class CryptoClass : IDisposable
     {
+        Aes Aes;
+
+        RSACryptoServiceProvider Rsa;
+
         public byte[] AesKey { get; set; }
+
+        public string RsaPrivateKey { get; set; }
+
+        public string RsaPublicKey { get; set; }
 
 
         public CryptoClass()
         {
+            Aes = Aes.Create("AES");
+
             AesKey = GenerateKey();
+
+            RsaPrivateKey = GenerateKeyRsa()[0];
+            RsaPublicKey = GenerateKeyRsa()[1];
         }
 
         public byte[] Encrypt(byte[] data)
@@ -23,21 +36,19 @@ namespace FileSend.Classes
             if (data == null)
                 return new byte[0];
 
-            using (var aes = Aes.Create())
+            using (var ms = new MemoryStream())
             {
-                using (var ms = new MemoryStream())
-                {                   
-                    var encryptor = aes.CreateEncryptor(AesKey, aes.IV);
-                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-                    {
-                        cs.Write(BitConverter.GetBytes(data.Length), 0, 4);
-                        cs.Write(data, 0, data.Length);
-                        cs.FlushFinalBlock();
-                        ms.Write(aes.IV, 0, aes.IV.Length);
-                    }                    
-                    return ms.ToArray();
+                var encryptor = Aes.CreateEncryptor(AesKey, Aes.IV);
+                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(BitConverter.GetBytes(data.Length), 0, 4);
+                    cs.Write(data, 0, data.Length);
+                    cs.FlushFinalBlock();
+                    ms.Write(Aes.IV, 0, Aes.IV.Length);
                 }
+                return ms.ToArray();
             }
+
         }
 
         public byte[] Decrypt(byte[] data)
@@ -53,28 +64,62 @@ namespace FileSend.Classes
 
             using (var ms = new MemoryStream(data))
             {
-                using (Aes aes = Aes.Create())
+                var decryptor = Aes.CreateDecryptor(AesKey, IV);
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
                 {
-                    var decryptor = aes.CreateDecryptor(AesKey, IV);
-                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    using (var br = new BinaryReader(cs))
                     {
-                        using (var br = new BinaryReader(cs))
-                        {
-                            int count = br.ReadInt32();
-                            return br.ReadBytes(count);
-                        }
+                        int count = br.ReadInt32();
+                        return br.ReadBytes(count);
                     }
                 }
             }
         }
 
+        public byte[] EncryptRsa(byte[] data)
+        {
+            if (data == null)
+                return new byte[0];
+
+            Rsa.FromXmlString(RsaPublicKey);
+            return Rsa.Encrypt(data, true);
+
+        }
+
+        public byte[] DecryptRsa(byte[] data)
+        {
+            if (data == null)
+                return new byte[0];
+
+            Rsa.FromXmlString(RsaPrivateKey);
+            return Rsa.Decrypt(data, true);
+
+        }
+
         public byte[] GenerateKey()
         {
-            using (Aes aes = Aes.Create())
-            {
-                aes.GenerateKey();
-                return aes.Key;
-            }
+            Aes.GenerateKey();
+            return Aes.Key;
+        }
+
+        public string[] GenerateKeyRsa()
+        {
+            if (Rsa != null)
+                Rsa.Dispose();
+
+            Rsa = new RSACryptoServiceProvider(2048);
+
+            string[] result = new string[2];
+            result[0] = Rsa.ToXmlString(true);
+            result[1] = Rsa.ToXmlString(false);
+
+            return result;
+        }
+
+        public void Dispose()
+        {
+            Rsa.Dispose();
+            Aes.Dispose();
         }
     }
 }
--- Tests/CryptoTest.cs
+++ Tests/CryptoTest.cs
@@ -16,7 +16,7 @@ namespace Tests
             var key1 = crypto.GenerateKey();
             var key2 = crypto.GenerateKey();
 
-            Assert.IsTrue(!key1.Equals(key2), "Wrong keys generator!");
+            Assert.IsTrue(!key1.Equals(key2), "Wrong Aes keys generator!");
 
             crypto.AesKey = key1;
 
@@ -25,7 +25,28 @@ namespace Tests
             byte[] encryptData = crypto.Encrypt(Encoding.UTF8.GetBytes(message));
             byte[] decryptData = crypto.Decrypt(encryptData);
 
-            Assert.AreEqual(message, Encoding.UTF8.GetString(decryptData), "Encrypt and Decrypt aren't working! EncryptData Length: " + encryptData.Length);
+            Assert.AreEqual(message, Encoding.UTF8.GetString(decryptData), "Aes doesn't work!");
+        }
+
+        [TestMethod]
+        public void RsaTests()
+        {
+            CryptoClass crypto = new CryptoClass();
+
+            var key1 = crypto.GenerateKeyRsa();
+            var key2 = crypto.GenerateKeyRsa();
+
+            Assert.IsTrue(!key1.Equals(key2), "Wrong Rsa keys generator!");
+
+            crypto.RsaPrivateKey = key1[0];
+            crypto.RsaPublicKey = key1[1];
+
+            String message = "Hello, world";
+
+            byte[] encryptData = crypto.EncryptRsa(Encoding.UTF8.GetBytes(message));
+            byte[] decryptData = crypto.DecryptRsa(encryptData);
+
+            Assert.AreEqual(message, Encoding.UTF8.GetString(decryptData), "Rsa doesn't work!");
         }
     }
 }
